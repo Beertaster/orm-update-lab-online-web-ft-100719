552a80e0a6573bcc2cb000afd9610b1f178d5e7e
William Stevens <wstevens.hornet@gmail.com>
2019-11-15 22:49:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,9 +15,18 @@ class Student
   end
 
   def self.create_table 
-    
-    
-    
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+      )
+    SQL
+    DB[:conn].execute(sql
   end
+  
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE 
 
 end
