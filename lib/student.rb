00712e6a82296a947b15304d05d664f970b499b8
William Stevens <wstevens.hornet@gmail.com>
2019-11-16 02:32:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,7 +57,18 @@ class Student
     student
   end
   
-  def self.find_name_
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE name = ?
+      LIMIT 1
+      SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+  
   
   
   
