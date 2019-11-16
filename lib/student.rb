aa7d74718773b5e78a40f563c9168e80e79f152f
William Stevens <wstevens.hornet@gmail.com>
2019-11-16 02:35:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -81,7 +81,15 @@ class Student
     end.first
   end
   
-  
+  def update
+    sql = <<-SQL
+      UPDATE students
+      SET name = ?, grade = ?
+      WHERE id = ?
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
   
   
   
